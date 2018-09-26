406d868d5971e6fe31e9fcb7d1627438ca1a2bb2
Tajir Hasnain <31435079+Tajir-Hasnain@users.noreply.github.com>
2018-09-26 17:58:22 +0600

    Update Sparse Table.cpp

--- Data Structures/Sparse Table.cpp
+++ Data Structures/Sparse Table.cpp
@@ -19,17 +19,17 @@ int main() {
 		sparse[i][0] = a[i];
 
 
-	for(int j = 1; pow(2,j) <= n; ++j) {
+	for(int j = 1; (1<<j) <= n; ++j) {
 		
 		//i + pow(2,j) -1  ==> as the highest range can't exceed the last index;
 
 		// min(0,5) in 0,1,2,3,4 is  min( min(0,3) , min(4,5) );
 
-		for(int i = 0; i+pow(2,j)-1 < n; ++i) {
+		for(int i = 0; i+( 1 << (j-1) ) < n; ++i) {
 
 			//operation here with the previously calculated 2 nodes
 			
-			sparse[i][j] = min(sparse[ i ][ j - 1 ] , sparse[ i + (int)pow(2,j) - 1 ][ j - 1 ] );	
+			sparse[i][j] = min(sparse[ i ][ j - 1 ] , sparse[ i + (1 << (j-1)) ][ j - 1 ] );	
 		}
 	} 
 
@@ -44,7 +44,7 @@ int main() {
 	int l = hi - lo + 1;	
 	int k = log2(l);
 
-	ans = min(sparse[ lo ][ k ] , sparse[ lo + l - (int)pow(2,k) ][ k ] );
+	ans = min(sparse[ lo ][ k ] , sparse[ lo + l - (1 << k ) ][ k ] );
 
 	//----Query ends
 
