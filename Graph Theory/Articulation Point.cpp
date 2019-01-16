2d2be2bb4cee1e4fc2447f52b35e55fcdc35e321
Tajir Hasnain <31435079+Tajir-Hasnain@users.noreply.github.com>
2019-01-16 23:41:07 +0600

    Update Articulation Point.cpp

--- Graph Theory/Articulation Point.cpp
+++ Graph Theory/Articulation Point.cpp
@@ -4,6 +4,7 @@ private:
 	vector < bool > vis;
 	vector < int > tin,fup;
 	set < int > art_point;
+	set < pair < int , int > > art_bridge;
 	vector < vector < int > > adj;
 
 	void dfs(int u, int p = -1) {
@@ -11,15 +12,17 @@ private:
 		tin[u] = fup[u] = timer++;
 		int children = 0;
 
-		for(auto v : graph[u]) {
+		for(auto v : adj[u]) {
 			if(v == p)	continue;
 			if(vis[v]) 
 				fup[u] = min(fup[u] , tin[v]);
 			else {
 				dfs(v,u);
 				fup[u] = min(fup[u] , fup[v]);
-				if(fup[v]>= tin[u] && p != -1) 
+				if(fup[v] > tin[u] && p != -1)  {
 					art_point.insert(u);
+					art_bridge.insert(make_pair(u,v));
+				}
 				++children;
 			}
 		}
@@ -28,7 +31,7 @@ private:
 	}
 
 	void find_cutpoints(vector < vector < int > > graph) {
-		for(int i = 0; i < n; ++i) {
+		for(int i = 0; i < node; ++i) {
 			if(!vis[i])
 				dfs(i);
 		}
@@ -43,10 +46,14 @@ public:
 		tin.assign(n,-1);
 		fup.assign(n,-1);
 
-		find_cutpoints();
+		find_cutpoints(graph);
 	}
 
 	set < int > getPoints() {
 		return art_point;
+	}
+
+	set < pair < int , int > > getBridges() {
+		return art_bridge;
 	}
 };
