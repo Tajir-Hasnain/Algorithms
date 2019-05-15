979ef86f642583684d00b03ad543e01b4d4271b3
Tajir Hasnain <31435079+Tajir-Hasnain@users.noreply.github.com>
2019-05-15 22:42:10 +0600

    Update Disjoint-Set Union.cpp

--- Data Structures/Disjoint-Set Union.cpp
+++ Data Structures/Disjoint-Set Union.cpp
@@ -5,15 +5,17 @@
     Usage:
         - Find (x) returns the set containing x
         - Union (x, y) joins two sets containing x and y, returns false if x and y are in the same set
+        - vector count stores how many nodes are connected with a parent. For any node u, the number of nodes connected with it is:
+            * dsu.count[ dsu.Find(u) ]
     Input:
         - n, number of sets
 */
 
 struct DisjointSet {
     int n;
-    vector <int> rank, parent;
+    vector <int> rank, parent,count;
 
-    DisjointSet (int n): n(n), rank(n, 0) {
+    DisjointSet (int n): n(n), rank(n, 0) , count(n,1) {
         for (int i = 0; i < n; i++) {
             parent.push_back(i);
         }
@@ -29,9 +31,11 @@ struct DisjointSet {
             return false;
         } else if (rank[x] < rank[y]) {
             parent[x] = y;
+            count[y]++;
         } else {
             parent[y] = x;
             rank[x] = max(rank[x], rank[y] + 1);
+            count[x]++;
         }
         return true;
     }
