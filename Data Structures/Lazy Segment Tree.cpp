d3d8b1661f179a5c9a8e765dbfdaa5251a96be83
Tajir Hasnain <31435079+Tajir-Hasnain@users.noreply.github.com>
2019-08-02 21:56:38 +0600

    Update Lazy Segment Tree.cpp

--- Data Structures/Lazy Segment Tree.cpp
+++ Data Structures/Lazy Segment Tree.cpp
@@ -46,7 +46,8 @@ void update(ll node, ll left, ll right, ll l, ll r, ll val) {
         update(lc,left,tm,l,r,val);
         update(rc,tm+1,right,l,r,val);
     }
-    st[node] = st[lc] + st[rc];     //Change here
+    //Change here
+    st[node] = st[lc] + st[rc];
 }
 
 ll query(ll node, ll left, ll right, ll l, ll r) {
@@ -55,10 +56,11 @@ ll query(ll node, ll left, ll right, ll l, ll r) {
     push(node,left,right);
 
     ll tm = (left+right)>>1;
-    
+
     //Change here
     
     if(r<= tm)    return query(lc, left, tm, l, r);
     else if(l > tm)    return query(rc, tm+1, right, l, r);
     else
         return ( query(lc, left, tm, l , r) + query(rc, tm+1, right, l, r) );
+}
